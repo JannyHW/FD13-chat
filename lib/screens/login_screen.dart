2b5a6a13234dd61f904178f1eb6be805b1cf0342
Jongdee H <jannykosin@gmail.com>
2021-04-21 21:16:11 -0700

    incomplete: android-firebase issues

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:fd13/components.dart/rounded_button.dart';
+import 'chat_screen.dart';
+import 'package:fd13/constants.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              title: 'Log In',
+              colour: Color(0xFF8ac4d0),
+              onPressed: () {
+                Navigator.pushNamed(context, ChatScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
